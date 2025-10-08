8d29d5ff296fc8afb23fea7090db5be3f7aab68a
Kirill <68159423+Kirill-Mops@users.noreply.github.com>
2025-10-09 00:47:37 +0300

    Add files via upload

---
+++ Лабораторная №1/University2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+#define PI 3.14159
+using namespace std;
+
+int main()
+{
+    float x, y, R, S, C;
+    cin >> x >> y;
+    R = x * tan(PI / 2);
+    S = sqrt(pow(x, 2)+pow(y, 2))/(pow(2, log2(x)));
+    cout << R << " " << S << endl;
+    C = max(R, S);
+    cout << C << endl;
+
+    system("pause");
+
+    return 0;
+}
