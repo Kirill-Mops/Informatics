e184e35d6e95fb90403b012d3fc4d0b4665bada2
Kirill <68159423+Kirill-Mops@users.noreply.github.com>
2026-02-28 09:32:53 +0300

    Add files via upload

---
+++ Алгоритмизация/Лабораторная №2/UnitTest1.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../homework/homework.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int n = 10;
+			double result1 = simpson_rule(function1, 5, 6, n);
+			double result2 = simpson_rule(function2, 5, 6, n);
+			double result = result1 - result2;
+			Assert::AreEqual(115.333, result, 0.1, L"Значения не совпали");
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			int n = 100;
+			double result1 = simpson_rule(function1, 5, 6, n);
+			double result2 = simpson_rule(function2, 5, 6, n);
+			double result = result1 - result2;
+			Assert::AreEqual(115.333, result, 0.1, L"Значения не совпали");
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			int n = 1000;
+			double result1 = simpson_rule(function1, 5, 6, n);
+			double result2 = simpson_rule(function2, 5, 6, n);
+			double result = result1 - result2;
+			Assert::AreEqual(115.333, result, 0.1, L"Значения не совпали");
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			int n = 10000;
+			double result1 = simpson_rule(function1, 5, 6, n);
+			double result2 = simpson_rule(function2, 5, 6, n);
+			double result = result1 - result2;
+			Assert::AreEqual(115.333, result, 0.1, L"Значения не совпали");
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			int n = 100000;
+			double result1 = simpson_rule(function1, 5, 6, n);
+			double result2 = simpson_rule(function2, 5, 6, n);
+			double result = result1 - result2;
+			Assert::AreEqual(115.333, result, 0.1, L"Значения не совпали");
+		}
+
+	};
+}
---
+++ Алгоритмизация/Лабораторная №2/homework.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+double simpson_rule(double (*f)(double), double a, double b, int n) {
+    if (n % 2 == 1) n++; // Делаем n четным
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+
+    for (int i = 1; i < n; i += 2)
+        sum += 4 * f(a + i * h);
+
+    for (int i = 2; i < n - 1; i += 2)
+        sum += 2 * f(a + i * h);
+
+    return (h / 3) * sum;
+}
+
+double function1(double x) {
+    return 2 * pow(x, 2) + 1;
+}
+
+double function2(double x) {
+    return -2 * pow(x, 2) + 7; 
+}
+
+int main() {
+    for (int n = 1;  n < 10001; n*=10) {
+        double result1 = simpson_rule(function1, 5, 6, n);
+        double result2 = simpson_rule(function2, 5, 6, n);
+        double result = result1 - result2;
+        std::cout << "Approximate value of the integral: " << result << std::endl;
+    }
+    return 0;
+}
