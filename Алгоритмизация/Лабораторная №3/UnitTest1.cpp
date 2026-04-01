1e1ec54c6b71888fd245ccd898b78c5d1aa2ee1b
Kirill <68159423+Kirill-Mops@users.noreply.github.com>
2026-04-01 23:04:07 +0300

    Add files via upload

---
+++ Алгоритмизация/Лабораторная №3/UnitTest1.cpp
@@ -0,0 +1,89 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../homework/homework.cpp"
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			std::vector<int> vec1 = {2, 1 ,3};
+			std::vector<int> exp1 = {1, 2, 3};
+			std::vector<int> act1 = MergeSort(vec1);
+			std::vector<int> vec2 = { 6, 8, 1, 2, 3 };
+			std::vector<int> exp2 = { 1, 2, 3, 6, 8 };
+			std::vector<int> act2 = MergeSort(vec2);
+			std::vector<int> vec3 = { 4, 10, 8, 123 };
+			std::vector<int> exp3 = { 4, 8, 10, 123 };
+			std::vector<int> act3 = MergeSort(vec3);
+			std::vector<int> vec4 = { 456, 23, 0, 7 };
+			std::vector<int> exp4 = { 0, 7, 23, 456 };
+			std::vector<int> act4 = MergeSort(vec4);
+			std::vector<int> vec5 = { 90, 54, 85, 123 };
+			std::vector<int> exp5 = { 54, 85, 90, 123 };
+			std::vector<int> act5 = MergeSort(vec5);
+
+			Assert::IsTrue(vec1 == exp1, L"Значения не совпали");
+			Assert::IsTrue(vec2 == exp2, L"Значения не совпали");
+			Assert::IsTrue(vec3 == exp3, L"Значения не совпали");
+			Assert::IsTrue(vec4 == exp4, L"Значения не совпали");
+			Assert::IsTrue(vec5 == exp5, L"Значения не совпали");
+		}
+		
+		TEST_METHOD(TestMethod2)
+		{
+			std::vector<int> vec1 = { 2, 1 ,3 };
+			std::vector<int> exp1 = { 1, 2, 3 };
+			std::vector<int> act1 = RadixSort(vec1);
+			std::vector<int> vec2 = { 6, 8, 1, 2, 3 };
+			std::vector<int> exp2 = { 1, 2, 3, 6, 8 };
+			std::vector<int> act2 = RadixSort(vec2);
+			std::vector<int> vec3 = { 4, 10, 8, 123 };
+			std::vector<int> exp3 = { 4, 8, 10, 123 };
+			std::vector<int> act3 = RadixSort(vec3);
+			std::vector<int> vec4 = { 456, 23, 0, 7 };
+			std::vector<int> exp4 = { 0, 7, 23, 456 };
+			std::vector<int> act4 = RadixSort(vec4);
+			std::vector<int> vec5 = { 90, 54, 85, 123 };
+			std::vector<int> exp5 = { 54, 85, 90, 123 };
+			std::vector<int> act5 = RadixSort(vec5);
+
+			Assert::IsTrue(vec1 == exp1, L"Значения не совпали");
+			Assert::IsTrue(vec2 == exp2, L"Значения не совпали");
+			Assert::IsTrue(vec3 == exp3, L"Значения не совпали");
+			Assert::IsTrue(vec4 == exp4, L"Значения не совпали");
+			Assert::IsTrue(vec5 == exp5, L"Значения не совпали");
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			std::vector<int> vec1 = { 2, 1 ,3 };
+			std::vector<int> exp1 = { 1, 2, 3 };
+			std::vector<int> act1 = make_Quick_sort(vec1);
+			std::vector<int> vec2 = { 6, 8, 1, 2, 3 };
+			std::vector<int> exp2 = { 1, 2, 3, 6, 8 };
+			std::vector<int> act2 = make_Quick_sort(vec2);
+			std::vector<int> vec3 = { 4, 10, 8, 123 };
+			std::vector<int> exp3 = { 4, 8, 10, 123 };
+			std::vector<int> act3 = make_Quick_sort(vec3);
+			std::vector<int> vec4 = { 456, 23, 0, 7 };
+			std::vector<int> exp4 = { 0, 7, 23, 456 };
+			std::vector<int> act4 = make_Quick_sort(vec4);
+			std::vector<int> vec5 = { 90, 54, 85, 123 };
+			std::vector<int> exp5 = { 54, 85, 90, 123 };
+			std::vector<int> act5 = make_Quick_sort(vec5);
+
+			Assert::IsTrue(vec1 == exp1, L"Значения не совпали");
+			Assert::IsTrue(vec2 == exp2, L"Значения не совпали");
+			Assert::IsTrue(vec3 == exp3, L"Значения не совпали");
+			Assert::IsTrue(vec4 == exp4, L"Значения не совпали");
+			Assert::IsTrue(vec5 == exp5, L"Значения не совпали");
+		}
+	};
+}
---
+++ Алгоритмизация/Лабораторная №3/homework.cpp
@@ -0,0 +1,161 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+
+template <typename Func, typename... Args>
+auto measure_execution_time(Func&& func, Args&&... args) {
+    auto start_time = std::chrono::high_resolution_clock::now();  // Начинаем отсчет времени
+    auto result = func(std::forward<Args>(args)...);  // Выполняем переданную функцию с аргументами
+    auto end_time = std::chrono::high_resolution_clock::now();  // Останавливаем отсчет времени
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);  // Вычисляем продолжительность
+    return std::make_pair(result, duration.count());  // Возвращаем результат и время выполнения
+}
+
+std::vector<int> MergeSort(std::vector<int>& vec) {
+    if (vec.size() > 1)
+    {
+        int mid = vec.size() / 2;
+        std::vector<int> left(vec.begin(), vec.begin() + mid);
+        std::vector<int> right(vec.begin() + mid, vec.end());
+
+        MergeSort(left);
+        MergeSort(right);
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+
+        while ((i < left.size()) and (j < right.size()))
+        {
+            if (left[i] < right[j])
+            {
+                vec[k] = left[i];
+                i++;
+            }
+            else
+            {
+                vec[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < left.size()) {
+            vec[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.size())
+        {
+            vec[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    return vec;
+}
+
+std::vector<int> UnionBuckets(std::vector<std::vector<int>>& buckets) {
+    std::vector<int> UnionVec;
+    for (std::vector<int>& bucket : buckets) {
+        UnionVec.insert(UnionVec.end(), bucket.begin(), bucket.end());
+    }
+    return UnionVec;
+}
+
+void sortRad(std::vector<int>& vec, int& Rad) {
+    std::vector<std::vector<int>> buckets(10);
+
+    for (int value : vec)
+    {
+        int indBacket = (value / Rad) % 10;
+        buckets[indBacket].push_back(value);
+    }
+    vec = UnionBuckets(buckets);
+}
+
+std::vector<int> RadixSort(std::vector<int>& vec) {
+    int maxValue = *std::max_element(vec.begin(), vec.end());
+    int Rad = 1;
+    while (maxValue / Rad > 0)
+    {
+        sortRad(vec, Rad);
+        Rad = Rad * 10;
+    }
+
+    return vec;
+}
+
+int Slicer(std::vector<int>& vec, int down, int high)
+{
+    int main = vec[high];
+    int i = down - 1;
+    for (int j = down; j <= high - 1; j++)
+    {
+        if (vec[j] <= main)
+        {
+            i++;
+            std::swap(vec[i], vec[j]);
+        }
+    }
+    std::swap(vec[i + 1], vec[high]);
+    return i + 1;
+}
+
+void QuickSort(std::vector<int>& vec, int down, int high) {
+    if (down < high)
+    {
+        int mainIndex = Slicer(vec, down, high);
+        QuickSort(vec, down, mainIndex - 1);
+        QuickSort(vec, mainIndex + 1, high);
+    }
+}
+
+std::vector<int> make_vector(int& n) {
+    std::vector<int> vec(n);
+    for (int i = 0; i < vec.size(); ++i) {
+        vec[i] = std::rand() % 1000000;
+    }
+    return vec;
+}
+
+std::vector<int> make_Quick_sort(std::vector<int>& vec) {
+    QuickSort(vec, 0, vec.size() - 1);
+    return vec;
+}
+
+int main() {
+
+    int sizes[] = { 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000 };
+
+    for (int n : sizes) {
+        std::vector<int> vec = make_vector(n);
+        auto [result, execution_time] = measure_execution_time(MergeSort, vec);
+        std::cout << execution_time << std::endl;
+    }
+
+    std::cout << "" << std::endl;
+
+    for (int n : sizes) {
+        std::vector<int> vec = make_vector(n);
+        auto [result, execution_time] = measure_execution_time(RadixSort, vec);
+        std::cout << execution_time << std::endl;
+    }
+
+    std::cout << "" << std::endl;
+
+    for (int n : sizes) {
+        std::vector<int> vec = make_vector(n);
+        auto [result, execution_time] = measure_execution_time(make_Quick_sort, vec);
+        std::cout << execution_time << std::endl;
+    }
+
+    return 0;
+}
