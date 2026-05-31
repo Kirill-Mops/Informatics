3033f64c6e7303698e5049a8a5fe4d85699cb276
Kirill <68159423+Kirill-Mops@users.noreply.github.com>
2026-05-31 13:41:26 +0300

    Add files via upload

---
+++ Алгоритмизация/Лабораторная №7/Разделяй и властвуй/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../homework/homework.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+		TEST_METHOD(Test1)
+		{
+			std::vector<int> nums = { 2, 5, 5, 5, 6, 6, 8, 9, 9, 9 };
+			int target = 5;
+			int exp = 1;
+			int act = find(nums, target, true);
+			Assert::AreEqual(exp, act);
+		}
+
+		TEST_METHOD(Tes2)
+		{
+			std::vector<int> nums = { 2, 5, 5, 5, 6, 6, 8, 9, 9, 9 };
+			int target = 5;
+			int exp = 3;
+			int act = find(nums, target, false);
+			Assert::AreEqual(exp, act);
+		}
+
+		TEST_METHOD(Test3)
+		{
+			std::vector<int> nums = { 2, 5, 5, 5, 6, 6, 8, 9, 9, 9 };
+			int target = 4;
+			int exp = -1;
+			int act = find(nums, target, false);
+			Assert::AreEqual(exp, act);
+		}
+	};
+}
---
+++ Алгоритмизация/Лабораторная №7/Разделяй и властвуй/homework.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+
+int find(const std::vector<int>& nums, int target, bool find_first) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int result = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            result = mid; 
+            if (find_first) {
+                right = mid - 1; 
+            }
+            else {
+                left = mid + 1;  
+            }
+        }
+        else if (nums[mid] > target) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+
+    return result;
+}
+
+void solve(const std::vector<int>& nums, int target) {
+    int first = find(nums, target, true);
+
+    if (first == -1) {
+        std::cout << "Element not found in the array" << std::endl;
+        return;
+    }
+
+    int last = find(nums, target, false);
+    std::cout << "The first occurrence of element " << target << " is located at index " << first << std::endl;
+    std::cout << "The last occurrence of element " << target << " is located at index " << last << std::endl;
+}
+
+int main() {
+    std::vector<int> nums1 = { 2, 5, 5, 5, 6, 6, 8, 9, 9, 9 };
+    int target1 = 5;
+    solve(nums1, target1);
+
+    std::vector<int> nums2 = { 2, 5, 5, 5, 6, 6, 8, 9, 9, 9 };
+    int target2 = 4;
+    solve(nums2, target2);
+
+    return 0;
+}
